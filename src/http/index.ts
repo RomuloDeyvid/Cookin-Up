6f08ae195b1ad8bf67ed218cd3fbd53ef83dde95
Rômulo Deyvid <120958836+RomuloDeyvid@users.noreply.github.com>
2024-08-17 07:56:25 -0300

    Update index.ts

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,9 +7,9 @@ async function obterDadosURL<T>(url: string) {
 }
 
 export async function obterCategorias() {
-  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/RomuloDeyvid/698a30717060bc3648176b4957bee82c/raw/e42e8652c87e1206b244d328ea5d145b45949af7/categorias.json');
 }
 
 export async function obterReceitas() {
-  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
+  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/RomuloDeyvid/690b00648af7a3def50ba0d6157160e7/raw/42330a42db148081fea502e0817d66b4cf6e6544/receitas.json');
 }
